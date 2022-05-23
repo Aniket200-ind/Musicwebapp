282f677660165a4980c38fb5adc50032745cb769
Aniket200-ind <aniketbotre007@gmail.com>
2022-05-23 17:59:35 +0530

    Music player

---
+++ js/music-list.js
@@ -0,0 +1,224 @@
+let allMusic = [
+    {
+        name: "Alone",
+        artist: "Alan Walker",
+        img: "img-1",
+        src: "music-1"
+    },
+    {
+        name: "Alone Pt.2",
+        artist: "Alan Walker",
+        img: "img-2",
+        src: "music-2"
+    },
+    {
+        name: "All Falls Down",
+        artist: "Alan Walker",
+        img: "img-3",
+        src: "music-3"
+    },
+    {
+        name: "Darkside",
+        artist: "Alan Walker",
+        img: "img-4",
+        src: "music-4"
+    },
+    {
+        name: "Different World",
+        artist: "Alan Walker",
+        img: "img-5",
+        src: "music-5"
+    },
+    {
+        name: "Faded",
+        artist: "Alan Walker",
+        img: "img-6",
+        src: "music-6"
+    },
+    {
+        name: "Paradise",
+        artist: "Alan Walker",
+        img: "img-7",
+        src: "music-7"
+    },
+    {
+        name: "Sing me to sleep",
+        artist: "Alan Walker",
+        img: "img-8",
+        src: "music-8"
+    },
+    {
+        name: "The Drum",
+        artist: "Alan Walker",
+        img: "img-9",
+        src: "music-9"
+    },
+    {
+        name: "Tired",
+        artist: "Alan Walker",
+        img: "img-10",
+        src: "music-10"
+    },
+    {
+        name: "World we used to know",
+        artist: "Alan Walker",
+        img: "img-11",
+        src: "music-11"
+    },
+    {
+        name: "Ignite",
+        artist: "Alan Walker",
+        img: "img-12",
+        src: "music-12"
+    },
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "img-13",
+        src: "music-13"
+    },
+    {
+        name: "Hymn for the weekend",
+        artist: "Coldplay",
+        img: "img-14",
+        src: "music-14"
+    },
+    {
+        name: "Let Me Love You",
+        artist: "DJ Snake",
+        img: "img-15",
+        src: "music-15"
+    },
+    {
+        name: "Taki Taki",
+        artist: "DJ Snake",
+        img: "img-16",
+        src: "music-16"
+    },
+    {
+        name: "Levitating",
+        artist: "Dua Lipa",
+        img: "img-17",
+        src: "music-17"
+    },
+    {
+        name: "Middle of the night",
+        artist: "Elley Duhe",
+        img: "img-18",
+        src: "music-18"
+    },
+    {
+        name: "Love me like yo do",
+        artist: "Ellie Goulding",
+        img: "img-19",
+        src: "music-19"
+    },
+    {
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        img: "img-20",
+        src: "music-20"
+    },
+    {
+        name: "I'm an albatraoz",
+        artist: "Aron Chupa",
+        img: "img-21",
+        src: "music-21"
+    },
+    {
+        name: "Believer",
+        artist: "Imagine Dragons",
+        img: "img-22",
+        src: "music-22"
+    },
+    {
+        name: "Jugnu",
+        artist: "Badshah",
+        img: "img-23",
+        src: "music-23"
+    },
+    {
+        name: "Industry Baby",
+        artist: "Lil Nas",
+        img: "img-24",
+        src: "music-24"
+    },
+    {
+        name: "In The End",
+        artist: "Tommee Profitt",
+        img: "img-25",
+        src: "music-25"
+    },
+    {
+        name: "Sugar",
+        artist: "Maroon 5",
+        img: "img-26",
+        src: "music-26"
+    },
+    {
+        name: "Payphone",
+        artist: "Maroon 5",
+        img: "img-27",
+        src: "music-27"
+    },
+    {
+        name: "Darkside",
+        artist: "Neoni",
+        img: "img-28",
+        src: "music-28"
+    },
+    {
+        name: "Pehli Nazar Mein",
+        artist: "Atif Aslam",
+        img: "img-29",
+        src: "music-29"
+    },
+    {
+        name: "Perfect",
+        artist: "Ed Sheeran",
+        img: "img-30",
+        src: "music-30"
+    },
+    {
+        name: "No Lie",
+        artist: "Sean Paul Ft.Dua Lipa",
+        img: "img-31",
+        src: "music-31"
+    },
+    {
+        name: "Cheap Thrills",
+        artist: "Sia Ft.Sean Paul",
+        img: "img-32",
+        src: "music-32"
+    },
+    {
+        name: "Make it bun dem",
+        artist: "Skrillex",
+        img: "img-33",
+        src: "music-33"
+    },
+    {
+        name: "Go Down Deh",
+        artist: "Sean Paul, Shaggy",
+        img: "img-34",
+        src: "music-34"
+    },
+    {
+        name: "Tera Hone Laga Hoon",
+        artist: "Atif Aslam, Arjit Singh",
+        img: "img-35",
+        src: "music-35"
+    },
+    {
+        name: "Closer",
+        artist: "Hasley",
+        img: "img-36",
+        src: "music-36"
+    },
+    {
+        name: "Dance Monkey",
+        artist: "Tones and I",
+        img: "img-37",
+        src: "music-37"
+    },
+]
